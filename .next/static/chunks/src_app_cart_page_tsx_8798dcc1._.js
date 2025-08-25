8692fbb8275c6070d5d8097e52f2a6c2282678fb
udemysher-byte <udemysher@gmail.com>
2025-08-25 16:24:51 +0100

    Add .next/static/chunks/src_app_cart_page_tsx_8798dcc1._.js 1756135490647

---
+++ .next/static/chunks/src_app_cart_page_tsx_8798dcc1._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_cart_page_tsx_4788d5c2._.js"
+],
+    source: "dynamic"
+});
