1fb24440832fd24232150e1f36b360f3426cc788
udemysher-byte <udemysher@gmail.com>
2025-08-25 16:24:42 +0100

    Add .next/static/chunks/src_app_contact_page_tsx_8798dcc1._.js 1756135481647

---
+++ .next/static/chunks/src_app_contact_page_tsx_8798dcc1._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_contact_page_tsx_20db083e._.js"
+],
+    source: "dynamic"
+});
