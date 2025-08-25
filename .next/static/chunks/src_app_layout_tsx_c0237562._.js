3cb7a24d5471bb227c338b6cebf8d92bb8a1715e
udemysher-byte <udemysher@gmail.com>
2025-08-25 16:25:43 +0100

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1756135542658

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_next_dist_c3a575e9._.js",
+  "static/chunks/src_components_37755de8._.js"
+],
+    source: "dynamic"
+});
